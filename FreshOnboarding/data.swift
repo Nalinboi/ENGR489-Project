3570721b8bb442fddc27688da9a75e2bcfbc70c6
Nalin Aswani <38874717+Nalinboi@users.noreply.github.com>
2021-06-30 13:34:53 +1200

    More resources, plus consistent image colours

--- FreshOnboarding/data.swift
+++ FreshOnboarding/data.swift
@@ -16,11 +16,21 @@ let tabs = [
 
 let mhresources = [
     Resource(title: "The Lowdown", description: "The Lowdown is a popular local mental health application. It has a list of topics and testimonials to choose from. The Lowdown has an experienced group of counsellors who are available 24/7", website: "www.thelowdown.co.nz"),
-    Resource(title: "Aunty Dee", description: "Here includes a description for aunty dee", website: "www.auntydee.co.nz")
+    Resource(title: "Aunty Dee", description: "Aunty Dee is a free online tool for anyone who needs some help working through a problem. It doesn’t matter what the problem is, you can use Aunty Dee to help you work it through.", website: "www.auntydee.co.nz"),
+    Resource(title: "Depression.org.nz" , description: "This website helps New Zealanders recognise and understand depression and anxiety. This website is part of a national public health programme, the National Depression Initiative. It includes The Journal – an online self-help programme.", website: "www.depression.org.nz"),
+    Resource(title: "Clearhead", description: "Clearhead is your private wellbeing assistant who will help you with learning about your mental health. Available for free, 24 hours a day, 7 days a week. Receive regular support and advice from your mobile phone or computer wherever you are, whenever you need it" , website: "www.clearhead.org.nz"),
+    Resource(title: "Ministry of Health", description: "Here you can find information and resources on how to cope with mental illness.", website: "www.health.govt.nz/your-health/conditions-and-treatments/mental-health"),
+//    Resource(title: , description: , website: )
 ]
 
 let mindfulnessTools = [
     Resource(title: "Mindful", description: "it is the mindful", website: "www.mindful.org"),
+    
+    Resource(title: "SPARX", description: "SPARX is an interactive self-help online tool that teaches young people skills to help combat depression and anxiety.", website: "www.mindful.org"),
+    
+    Resource(title: "Clearhead", description: "We provide you with specific and helpful resources for a range of situations we can all find ourselves in. We offer links to relevant websites, information sheets, books, videos and local support services to help with a wide range of mental health situations. Whatever you need help with, you will find something of value within our app and website." , website: "www.clearhead.org.nz")
+
+
 ]
 
 struct Page {
