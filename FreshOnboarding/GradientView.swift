5510d0eb474ab7014ee8db84c02e603e91d1c735
Nalin Aswani <38874717+Nalinboi@users.noreply.github.com>
2021-07-16 03:55:52 +1200

    Revert "normal"
    This reverts commit 355c354ec00f6e031d1eaf6d63685a6672d0bd05.

--- FreshOnboarding/GradientView.swift
+++ FreshOnboarding/GradientView.swift
@@ -10,18 +10,20 @@ import SwiftUI
 struct GradientView: View {
     @Binding var isWalkthroughShowing: Bool
     var body: some View {
-
-//        Gradient(colors: [Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.6312997341, green: 0.9606612325, blue: 1, alpha: 1))]),
+        // Can get different colors by doing "Color(color literal)" and an option should show up
+//        LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.6312997341, green: 0.9606612325, blue: 1, alpha: 1)), .white]),
+//                       startPoint: .top, endPoint: .bottom).ignoresSafeArea()
+//        LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.6312997341, green: 0.9606612325, blue: 1, alpha: 1)), .white]),
+//                       startPoint: .top, endPoint: .bottom).ignoresSafeArea()
         
         Group {
             if isWalkthroughShowing {
-                LinearGradient(gradient:
-                   Gradient(colors: [.white, Color(#colorLiteral(red: 0.6312997341, green: 0.9606612325, blue: 1, alpha: 1))]),
+                LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.6312997341, green: 0.9606612325, blue: 1, alpha: 1))]),
                                startPoint: .top, endPoint: .bottom).ignoresSafeArea()
             } else {
 //                LinearGradient(gradient: Gradient(colors: [.white, Color(#colorLiteral(red: 0.6312997341, green: 0.9606612325, blue: 1, alpha: 1))]),
 //                               startPoint: .top, endPoint: .bottom).ignoresSafeArea()
-                LinearGradient(gradient: Gradient(colors: [.white, Color(#colorLiteral(red: 0.6312997341, green: 0.9606612325, blue: 1, alpha: 1))]),
+                LinearGradient(gradient: Gradient(colors: [.white]),
                                startPoint: .top, endPoint: .bottom).ignoresSafeArea()
             }
         }
